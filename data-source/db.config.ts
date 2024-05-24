f037374f8696147763e407df2efc86d56f646afc
Narendran <naren.cse008@gmail.com>
2024-05-24 14:12:08 +0530

    update

--- data-source/db.config.ts
+++ data-source/db.config.ts
@@ -1,13 +1,22 @@
 import { DataSource } from "typeorm";
+import { Group } from "../models/Group";
+import { User } from "../models/User";
+import { GroupUser } from "../models/GroupUser";
+import { Balance } from "../models/Balance";
+import { Expense } from "../models/Expense";
 
 export const AppDataSource = new DataSource({
   type: "mssql",
   host: "localhost",
   port: 1433,
   username: "sa",
-  password: "test",
+  password: "",
   database: "test",
   synchronize: true,
-  logging: true,
-  entities: [],
+  logging: false,
+  entities: [Group, User, GroupUser, Balance, Expense],
+  options: {
+    trustServerCertificate: true,
+    encrypt: false,
+  },
 });
